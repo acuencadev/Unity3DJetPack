bb2c91fbefa95fb07a154a23bf2e306b7792907b
Amador Cuenca <sphi02ac@gmail.com>
2020-01-02 14:13:17 -0300

    Create the Electric Obstacle

---
+++ Jet Pack Replica/Assets/Scripts/ElectricObstacle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricObstacle : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(other.gameObject);
+            Debug.Log("Player died when collided with a electric obstacle.");
+        }
+    }
+}
