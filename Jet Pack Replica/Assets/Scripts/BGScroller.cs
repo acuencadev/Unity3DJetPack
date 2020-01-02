266cd6cf93231dba6126e74cc09eb8aa61592cce
Amador Cuenca <sphi02ac@gmail.com>
2020-01-02 15:03:21 -0300

    Prototype the BG scroller

---
+++ Jet Pack Replica/Assets/Scripts/BGScroller.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class BGScroller : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+}
