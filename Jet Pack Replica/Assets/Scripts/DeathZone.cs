33d85f5fb1f105d035f3344da459ad88ad9ee595
Amador Cuenca <sphi02ac@gmail.com>
2020-01-02 13:59:16 -0300

    Create the top and bottom Death Zones

---
+++ Jet Pack Replica/Assets/Scripts/DeathZone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(other.gameObject);
+            Debug.Log("Game Over!");
+        }
+    }
+}
