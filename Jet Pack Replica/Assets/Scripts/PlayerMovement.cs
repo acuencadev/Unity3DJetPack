c3f39f388de1c916b8ca926f0de4078d876e900c
Amador Cuenca <sphi02ac@gmail.com>
2020-01-02 13:50:17 -0300

    Flap the player

---
+++ Jet Pack Replica/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float forceMultiplier = 100f;
+
+    private Rigidbody2D rb2d;
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb2d.velocity = Vector3.zero;
+            rb2d.AddForce(Vector3.up * forceMultiplier);
+        }
+    }
+}
