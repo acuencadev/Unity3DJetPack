69a68ceb92d502de0a83a1bc8b503cf20d4760f1
Amador Cuenca <sphi02ac@gmail.com>
2020-01-03 17:42:08 -0300

    Improve the BG Collector logic

--- Jet Pack Replica/Assets/Scripts/BG/BGCollector.cs
+++ Jet Pack Replica/Assets/Scripts/BG/BGCollector.cs
@@ -3,31 +3,20 @@ using UnityEngine;
 
 public class BGCollector : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject backgroundPrefab;
-
-    [SerializeField]
-    private Transform parent;
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("BG"))
         {
-            Destroy(other.gameObject);
-            InstantiateNewBG();
+            RepositionBG(other.gameObject);
         }
     }
 
-    private Vector3 GetLastBgPosition()
+    private void RepositionBG(GameObject go)
     {
         var bg = GameObject.FindGameObjectsWithTag("BG").OrderByDescending(t => t.transform.position.x).First();
         var bgCollider = bg.GetComponent<BoxCollider2D>();
 
-        return (bgCollider == null) ? Vector3.zero : new Vector3(bg.transform.position.x + bgCollider.bounds.size.x, 0f, 0f);
+        go.transform.position = new Vector3(bg.transform.position.x + bgCollider.bounds.size.x, 0f, 0f);
     }
 
-    private void InstantiateNewBG()
-    {
-        GameObject newInstance = Instantiate(backgroundPrefab, GetLastBgPosition(), Quaternion.identity, parent);
-    }
 }
