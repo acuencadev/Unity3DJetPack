968665683c4074dcbda5384448adb55d5b946de0
Amador Cuenca <sphi02ac@gmail.com>
2020-01-04 13:07:52 -0300

    Create the generic looping system

---
+++ Jet Pack Replica/Assets/Scripts/Looping/SimpleLooper.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class SimpleLooper : MonoBehaviour
+{
+    [SerializeField]
+    private string matchingTag;
+
+    [SerializeField]
+    private GameObject prefab;
+
+    [SerializeField]
+    private Transform parent;
+
+    [SerializeField]
+    private int numOfInstances;
+
+    private void Start()
+    {
+        InstantiatePrefabs();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(matchingTag))
+        {
+            RepositionPrefab(other.gameObject);
+        }
+    }
+
+    private void InstantiatePrefabs()
+    {
+        float currentX = parent.position.x;
+
+        for (var i = 0; i < numOfInstances; i++)
+        {
+            Vector3 initialPosition = new Vector3(currentX, 0f, 0f);
+
+            GameObject newInstance = Instantiate(prefab, initialPosition, Quaternion.identity, parent);
+            BoxCollider2D newInstanceCollider = newInstance.GetComponent<BoxCollider2D>();
+
+            if (newInstanceCollider != null)
+            {
+                currentX = newInstance.transform.position.x + newInstanceCollider.bounds.size.x;
+            }
+        }
+    }
+
+    private void RepositionPrefab(GameObject go)
+    {
+        var bg = GameObject.FindGameObjectsWithTag(matchingTag).OrderByDescending(t => t.transform.position.x).First();
+        var bgCollider = bg.GetComponent<BoxCollider2D>();
+
+        go.transform.position = new Vector3(bg.transform.position.x + bgCollider.bounds.size.x, 0f, 0f);
+    }
+}
