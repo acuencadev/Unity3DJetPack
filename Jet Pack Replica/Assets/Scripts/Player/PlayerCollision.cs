5888d412b35be3179eadbe6aa5a053d4df4a40d1
Amador Cuenca <sphi02ac@gmail.com>
2020-01-06 17:01:32 -0300

    Refactor the player collision detection
    Move the collision detection to the player to improve performance.

---
+++ Jet Pack Replica/Assets/Scripts/Player/PlayerCollision.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+            Debug.Log("Player died when collided with a electric obstacle.");
+        }
+    }
+}
